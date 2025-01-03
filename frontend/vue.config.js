482c7f9fb7841b8888fb115a594ca6c236af4e18
Gage Chisholm <gagesmicrosoft@gmail.com>
2025-01-02 18:52:41 -0800

    Fixed Dist directory issues

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,4 +2,6 @@ module.exports = {
   devServer: {
     proxy: "http://localhost:5000",
   },
+  outputDir: "frontend/dist", // Ensures the build output goes to 'frontend/dist'
+  publicPath: "/", // Adjust if deploying to a subpath
 };
