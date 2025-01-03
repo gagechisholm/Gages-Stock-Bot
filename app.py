1f7e83246d0270a8ffa47792998e5c6b142a22bd
Gage Chisholm <gagesmicrosoft@gmail.com>
2025-01-02 19:08:04 -0800

    Fix R10 error with gunicorn

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, send_from_directory
 
 app = Flask(__name__, static_folder="frontend/dist", static_url_path="/")
@@ -12,4 +13,5 @@ def not_found(e):
     return send_from_directory(app.static_folder, "index.html")
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))  # Use Heroku's assigned port or 5000 for local
+    app.run(host="0.0.0.0", port=port)
